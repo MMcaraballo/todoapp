eb81f7eb6096bec387ea7d4621f45c4270f6127e
Mmcaraballo <miguelalejandroc123@gmail.com>
2023-01-16 23:46:07 -0400

    creacion del componente

---
+++ src/app/modules/auth/components/login-page/login-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // nombre de la etiqueta del componente
+  selector: 'app-login-page',
+  // contenido HTML o vista
+  templateUrl: './login-page.component.html',
+  // estilo del componente
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent {
+
+}
